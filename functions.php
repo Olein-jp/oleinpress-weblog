0a48f9576329f229970b868b3f49322a29a01377
KojiKuno <koji.kuno@gmail.com>
2019-01-09 14:25:13 +0900

    changed and fixed code directories

--- functions.php
+++ functions.php
@@ -127,9 +127,9 @@ function op_weblog_scripts() {
 
 	wp_enqueue_script( 'op-weblog-app-js', get_template_directory_uri() . '/assets/js/app.min.js', array( 'jquery' ), '20181208', true );
 
-	wp_enqueue_script( 'op-weblog-superfish', get_template_directory_uri() . '/src/assets/packages/superfish/js/superfish.min.js', array( 'jquery' ), '1.7.10', true );
+	wp_enqueue_script( 'op-weblog-superfish', get_template_directory_uri() . '/assets/packages/superfish/js/superfish.min.js', array( 'jquery' ), '1.7.10', true );
 
-	wp_enqueue_script( 'op-weblog-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
+	wp_enqueue_script( 'op-weblog-skip-link-focus-fix', get_template_directory_uri() . '/assets/js/skip-link-focus-fix.js', array(), '20151215', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
@@ -155,7 +155,7 @@ require get_template_directory() . '/inc/template-functions.php';
 /**
  * Customizer additions.
  */
-require get_template_directory() . '/inc/customizer.php';
+require get_template_directory() . '/inc/customizer/customizer.php';
 
 /**
  * Load Jetpack compatibility file.
