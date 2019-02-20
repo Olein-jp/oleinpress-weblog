aeb27f758c00ded3d09573f86e39bc6853e29610
KojiKuno <koji.kuno@gmail.com>
2019-02-20 13:26:18 +0900

    test self hosting theme.

--- inc/update/updater.php
+++ inc/update/updater.php
@@ -3,3 +3,27 @@
 use Inc2734\WP_GitHub_Theme_Updater\GitHub_Theme_Updater;
 
 $updater = new GitHub_Theme_Updater( get_template(), 'Olein-jp', 'oleinpress-weblog' );
+
+/**
+ * There is a case that comes back to GitHub's zip url.
+ * In that case it returns false because it is illegal.
+ */
+add_filter(
+	'inc2734_github_theme_updater_zip_url',
+	function( $url ) {
+		if ( 0 !== strpos( $url, 'https://olein-design.com/' ) ) {
+			return false;
+		}
+		return $url;
+	}
+);
+
+/**
+ * Customize request URL that for updating
+ */
+add_filter(
+	'inc2734_github_theme_updater_request_url',
+	function( $url ) {
+		return 'https://olein-design.com/webhooks/weblog/response.json';
+	}
+);
