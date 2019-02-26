20f29ddcea916848872f0738b5b69fcf2fe3492f
KojiKuno <koji.kuno@gmail.com>
2019-02-26 12:18:40 +0900

    アクセントカラーをカスタマイザーに追加 close #2

--- inc/customizer/panel-section-control.php
+++ inc/customizer/panel-section-control.php
@@ -1,23 +1,80 @@
 <?php
 use Inc2734\WP_Customizer_Framework\Framework;
 
+/*
+ * Remove default panel
+ */
+add_action( 'customize_register', 'op_remove_default_customizer_section' );
+function op_remove_default_customizer_section( $wp_customize ) {
+	// remove colors
+	$wp_customize->remove_section('colors');
+	// remove background image
+	$wp_customize->remove_section('background_image');
+	// remove header image
+	$wp_customize->remove_section('header_image');
+}
+
 if ( ! is_customize_preview() ) {
 	return;
 }
 
+/***
+ * Layout panel
+ * パネル：レイアウト
+ */
 Framework::panel(
-	'color',
+	'layout',
 	[
-		'title'    => __( 'color', 'oleinpress-weblog' ),
-		'priority' => 1000,
+		'title' => __( 'Layout', 'op-weblog' ),
+		'priority' => 1010,
 	]
 );
 
 Framework::section(
-	'base-design',
+	'archive-page',
 	[
-		'title'    => __( 'Base design settings', 'oleinpress-weblog' ),
-		'priority' => 100,
+		'title' => __( 'Page layout', 'op-weblog' ),
+		'description' => __( '', 'op-weblog' ),
+		'priority' => 130,
+	]
+);
+
+Framework::control(
+	'radio',
+	'archive-page-layout',
+	[
+		'label' => __( 'Page Layout','op-weblog' ),
+		'default' => 'one-column',
+		'choices' => array(
+			'one-column' => 'One column',
+			'two-column' => 'Two column',
+		),
+	]
+);
+
+$panel   = Framework::get_panel( 'layout' );
+$section = Framework::get_section( 'archive-page' );
+$control = Framework::get_control( 'archive-page-layout' );
+$control->join( $section )->join( $panel );
+
+/***
+ * Design panel
+ * パネル：デザイン
+ */
+Framework::panel(
+	'design',
+	[
+		'title' => __( 'Design', 'op-weblog' ),
+		'priority' => 1020,
+	]
+);
+
+Framework::section(
+	'basic',
+	[
+		'title' => __( 'Basic design', 'op-weblog' ),
+		'description' => __( '', 'op-weblog' ),
+		'priority' => 130,
 	]
 );
 
@@ -25,15 +82,13 @@ Framework::control(
 	'color',
 	'accent-color',
 	[
-		'label'    => __( 'Accent color', 'oleinpress-weblog' ),
-		'default'  => '#bd3c4f',
+		'label' => __( 'Accent color', 'op-weblog' ),
+		'default' => '#96ceb4',
 		'priority' => 100,
 	]
 );
-if ( ! is_customize_preview() ) {
-	return;
-}
-$panel   = Framework::get_panel( 'color' );
-$section = Framework::get_section( 'base-design' );
+
+$panel   = Framework::get_panel( 'design' );
+$section = Framework::get_section( 'basic' );
 $control = Framework::get_control( 'accent-color' );
 $control->join( $section )->join( $panel );
--- inc/customizer/style.php
+++ inc/customizer/style.php
@@ -1,2 +1,55 @@
 <?php
 use Inc2734\WP_Customizer_Framework\Style;
+
+add_action(
+	'inc2734_wp_customizer_framework_load_styles',
+	function() {
+		$accent_color = get_theme_mod( 'accent-color' );
+
+		Style::register(
+			[
+				'.l-site-header',
+				'.c-archive-thumbnail',
+				'.c-entry-thumbnail',
+				'.p-copyright',
+				'.widget-title',
+				'.c-entry-content table thead th',
+				'.widget_tag_cloud .tagcloud a',
+			],
+			[
+				"background: {$accent_color};",
+			]
+		);
+
+		Style::register(
+			[
+				'.c-entry-content h1',
+				'.c-entry-content h2',
+				'.c-entry-content h3',
+			],
+			[
+				"border-bottom-color: {$accent_color};",
+			]
+		);
+
+		Style::register(
+			[
+				'.c-entry-content h4',
+				'.c-entry-content h5',
+				'.c-entry-content h6',
+			],
+			[
+				"color: {$accent_color};",
+			]
+		);
+
+		Style::register(
+			[
+				'.widget_search .c-widget-searchform__submit',
+			],
+			[
+				"color: {$accent_color};",
+			]
+		);
+	}
+);
